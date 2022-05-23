47cf227d1c06c028c1f2e3333523de11c6086bd8
omstuhler <oscar.stuhler@gmx.de>
2022-05-23 00:54:17 -0400

    add_sentence string modification

--- R/retrieve_sentence.R
+++ R/retrieve_sentence.R
@@ -1,3 +1,5 @@
 retrieve_sentence = function(doc_i, sentence_i){
-  paste0(subset(tokens, tokens$doc_id == doc_i & tokens$sentence_id == sentence_i)$token, collapse = " ")
+  sent = paste0(subset(tokens, tokens$doc_id == doc_i & tokens$sentence_id == sentence_i)$token, collapse = " ")
+  sent = str_replace_all(sent, " (?=[,.;'])", "")
+  str_replace_all(sent, " {2,}", " ")
 }
