59aaf8cbca0e5401f498f0d30945b8d61382250e
omstuhler <oscar.stuhler@gmx.de>
2022-05-23 17:46:19 -0400

    -

--- R/semgram.R
+++
@@ -1,7 +0,0 @@
-#' semgram: A package for extracting semantically rich relations from textual data.
-#'
-#' semgram extracts semantic motifs from textual data. It builds on an entity-centered semantic grammar that distinguishes six classes of motifs: actions of an entity, treatments of an entity, agents acting upon an entity, patients acted upon by an entity, characterizations of an entity, and possessions of an entity. For reference, see Doe (XXXX).
-#'
-#' @docType package
-#' @name semgram
-NULL
